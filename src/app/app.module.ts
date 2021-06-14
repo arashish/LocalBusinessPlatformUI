52c5e49d00e400171e8238e14ea75b1cc1fcdec8
unknown <ar_ashish@hotmail.com>
2021-06-14 00:24:48 -0500

    Connected to the Springboot backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import {FormsModule} from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { CommonModule } from '@angular/common';  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
